dadc7a6d76bff6028fc123b3f8d6bd1ea48b29dd
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-07-07 10:20:58 -0500

    updated footer

--- layout/footers/footerThreeElements/FooterContactUsDetails.js
+++ layout/footers/footerThreeElements/FooterContactUsDetails.js
@@ -9,34 +9,6 @@ const FooterContactUsDetails = () => {
       <div className="footer-details text-center">
         <Logo />
         <p>Elegant retreat in a quiet Coral Gables setting. This home provides wonderful entertaining spaces with a chef kitchen opening</p>
-        <h6>Contact us</h6>
-        <ul className="icon-list">
-          <li>
-            <Link href="/contact/contact-us-3">
-              <i className="fas fa-map-marker-alt"></i>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact/contact-us-3">
-              <i className="fas fa-phone-alt"></i>
-            </Link>
-          </li>
-          <li>
-            <Link href="/pages/other-pages/signup">
-              <i className="fas fa-envelope"></i>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact/contact-us-3">
-              <i className="fas fa-globe"></i>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact/contact-us-3">
-              <i className="fas fa-wifi"></i>
-            </Link>
-          </li>
-        </ul>
       </div>
     </Col>
   );
