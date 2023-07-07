125cbe87a860d85832776039d6365c1fbc562d23
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-07-06 20:35:21 -0500

    removed icons

--- components/home/classic/PropertyServices.js
+++ components/home/classic/PropertyServices.js
@@ -4,11 +4,8 @@
  */
 import Link from "next/link";
 import React from "react";
-import Slider from "react-slick";
 import { Col, Container, Row } from "reactstrap";
 import { VendorServices, VendorServicesDesc } from "../../../constValues/constValues";
-import { serviceSlider } from "../../../data/slickSlider";
-import NoSsr from "../../../utils/NoSsr";
 
 const VendorServicesSection = ({ value }) => {
   return (
@@ -24,7 +21,7 @@ const VendorServicesSection = ({ value }) => {
               {value?.map((data, i) => (
                 <Col xl="4" md="6" className=" wow fadeInUp" key={i}>
                   <div className="service-box">
-                    <div className="icon-round">{data.img}</div>
+                    {/* <div className="icon-round">{data.img}</div> */}
                     <h3>
                       <Link href="/pages/other-pages/services">{data.title}</Link>
                     </h3>
