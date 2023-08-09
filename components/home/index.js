2ae4a65ab9287cf375c0870ad426846cd26ccebb
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-08-09 18:42:24 -0500

    removed unused call

--- components/home/index.js
+++ components/home/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { getData } from "../../utils/getData";
 import AboutSection from "./About";
 import VendorServicesSection from "./VendorServicesSection";
 import HomeBannerSection from "./HomeBanner";
@@ -8,15 +7,6 @@ import { HomePageData } from "../../data/pages/homePage";
 import { VendorServices } from "../../data/vendorServices";
 
 const BodyContent = () => {
-  const [value, setValue] = useState();
-
-  useEffect(() => {
-    getData(`${process.env.NEXT_PUBLIC_API_URL}/property`)
-      .then((res) => {
-        setValue(res.data);
-      })
-      .catch((error) => console.log("Error", error));
-  }, []);
 
   return (
     <>
