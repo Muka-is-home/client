7f41cbf09073a60af9748167425a537b2d9023ed
mukaadmin <138808236+mukaadmin@users.noreply.github.com>
2025-02-27 22:31:02 -0600

    Update footerData.js - updated footer links

--- data/footerData.js
+++ data/footerData.js
@@ -12,23 +12,23 @@ export const FooterData = {
     children: [
       { title: "Instagram", link: "https://www.instagram.com/mukaishome" },
       { title: "TikTok", link: "https://www.tiktok.com/@mukaishome" },
-      { title: "Facebook", link: "https://www.facebook.com/mukaishome" },
+      { title: "Facebook", link: "https://www.youtube.com/mukaishome" },
     ],
   },
   col3: {
     title: "Useful Links",
     children: [
       { title: "Become a Muka Realtor", link: `${process.env.NEXT_PUBLIC_URL}/adminapp/signup` },
-      { title: "Become a Muka Vendor", link: `${process.env.NEXT_PUBLIC_URL}/adminapp/signup` },
+      { title: "Become a Muka Expert", link: `${process.env.NEXT_PUBLIC_URL}/adminapp/signup` },
       { title: "Contact", link: "/contact" },
     ],
   },
   sub_footer: [ // find icons at font-awesome icons
-    { link: "https://www.facebook.com/mukaishome", class: "fab fa-facebook-f" },
+    { link: "https://www.youtube.com/mukaishome", class: "fab fa-youtube" },
     { link: "https://www.instagram.com/mukaishome", class: "fab fa-instagram" },
     { link: "https://www.tiktok.com/@mukaishome", class: "fab fa-tiktok" },
   ],
-  copyright: "All Right Reserved, Muka, LLC.",
+  copyright: "All Rights Reserved, Muka, LLC.",
   logo: "/assets/images/logo/muka-logo-teal.svg",
   logoWhite: "https://github-production-user-asset-6210df.s3.amazonaws.com/29741570/251847573-3ff39745-5d53-429c-a412-1f77336d1b65.png"
 };
