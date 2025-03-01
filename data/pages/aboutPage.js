eeba08b322041692393c15a277d6c719576a518c
mukaadmin <138808236+mukaadmin@users.noreply.github.com>
2025-02-28 22:08:00 -0600

    Update aboutPage.js - updated text

--- data/pages/aboutPage.js
+++ data/pages/aboutPage.js
@@ -1,13 +1,13 @@
 export const AboutPageData = {
-  mainHeading: "Muka is a virtual community dedicated to bridging the gap in minority homeownership",
+  mainHeading: "MUKA is a virtual community dedicated to bridging the gap in minority homeownership",
   subHeading1:
-    "When you join our community we are welcoming you into our family. We’re creating a safe place for you to get connected with the home buyers, sellers and trusted realtors that you’re looking for. We strive to provide you with the education and connections to make informed decisions that will set you up for lifetime success.",
+    "When you join our community you become part of our family. We’re creating a safe space where you can connect with trusted realtors, industry experts, and minority home vendors. Our goal is to provide you with the education, resources, and connections needed to make informed decisions and achieve long-term homeownership success.",
   subHeading2:
-    "At Muka, we are deeply aware of the persistent disparity between minority and white homeownership, which has unfortunately widened over the years. As disheartening as these statistics may be, they fuel our commitment to effect positive change. Inspired by the vision of a more inclusive and equitable real estate landscape, we have created Muka as a catalyst for closing this gap.",
+    "At MUKA, we recognize the longstanding disparity between minority and white homeownership - a gap that has only widened over the years. While these statistics are discouraging, they drive our unwavering commitment to change. MUKA was created as a catalyst for bridging this divide, fostering a more inclusive and equitable real estate landscape for all.",
   founderImg: "Founder Jantrice Johnson",
   founderHeading: "Built for you by a family of minority realtors.",
   founderText:
     "We're carrying on the legacy of the matriarch of our family who sowed a seed as an African-American realtor in the 1980's and 1990's. Muka is that seed fully bloomed.",
-  founderPara1: " Buying and selling a home is one of the biggest investments of your lifetime. But recent numbers show that minorities are at a 41% homeowner rate, compared to their counterparts. With Muka, the information and resources are now at your fingertips.",
-  founderPara2: " Buying and selling a home is one of the biggest investments of your lifetime. But recent numbers show that minorities are at a 41% homeowner rate, compared to their counterparts. With Muka, the information and resources are now at your fingertips."
+  founderPara1: "Buying and selling a home is not just a transaction—it’s one of the biggest milestones of your life, and I believe homeownership should be celebrated. It’s a symbol of achievement, security, and the foundation for building generational wealth. Sadly, the disparity in homeownership rates between minorities and their counterparts has persisted, with minorities at just a 41% homeownership rate. At MUKA, we aim to change that, offering you the tools and resources to take control of your journey toward homeownership.",
+  founderPara2: "Our mission at MUKA goes beyond just providing access to realtors, experts, and home vendors—we are here to empower you. We want to make sure you have all the knowledge and support you need to make decisions that will set you up for success. Homeownership is a dream worth striving for, and with MUKA, we’re committed to helping you achieve it."
 };
