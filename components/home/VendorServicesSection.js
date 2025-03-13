23c60ffff2801c5fcc4841b9f431c451cb651899
Muka Admin <138808236+mukaadmin@users.noreply.github.com>
2025-03-13 15:02:44 -0500

    Update VendorServicesSection.js - fix formatting of div href

--- components/home/VendorServicesSection.js
+++ components/home/VendorServicesSection.js
@@ -16,7 +16,7 @@ const VendorServicesSection = ({ value }) => {
             <Row className=" property-service column-space">
               {value?.map((data, i) => (
                 <Col xl="4" md="6" className=" wow fadeInUp" key={i}>
-                  <div className="service-box" onclick="location.href='/vendors?specialty=${data.tag_name}';">
+                  <div className="service-box" onclick={`location.href='/vendors?specialty=${data.tag_name}'`>
                     
                     {/*<div className="icon-round">{data.img}</div>*/}
                     <h3>
